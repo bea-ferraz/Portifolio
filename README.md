5d1657adb59ad6c24b93148f992408546cd62099
Beatriz Ferraz <79225626+bea-ferraz@users.noreply.github.com>
2021-10-17 17:44:31 -0300

    Finalizado

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Portifolio.github.io
-Meu portifólio
+<h1 align="center">
+  📰<br>Meu Portfólio
+</h1>
+<h4 align="center">
+  Portfólio criado com o intuito de mostrar meus projetos ao decorrer da minha carreira.
+</h4>
+
+  ### 💡 Ideia:
+
+ A ideia de desenvolver esse projeto surgiu após conhecer a comunidade do Discord "Codelândia" do Iuri Silva, a base que segui para desenvolver o portfolio está disponivel como um desafio, o link do mesmo no Figma é: https://www.figma.com/file/Yb9IBH56g7T1hdIyZ3BMNO/Desafios-Codel%C3%A2ndia?node-id=13190%3A2.
+ 
+ Após a ideia inicial proposta pela comunidade codelândia fiz algumas alterações e criei o meu portifolio personalizado.
+
+
+![Resultado final do projeto](assets/image/projeto.webm)
+
+<h4 align="center"><a href="https://bea-ferraz.github.io/Portifolio.github.io/">Clique para visitar o projeto</a></h4>
+
+## 📚 Seções
+O site é composto por 5 seções diferentes:
+
+- **Home:** Nele temos o meu avatar e uma breve apresentação;
+- **Quem sou:** Nessa seção tenho uma descrição dizendo um pouco sobre quem sou;
+- **Projetos:** Apresenta alguns projetos desenvolvidos e com link direto para os respectivos códigos no GitHub;
+- **Serviços:** Exibe através de cards os diferentes serviços em que possuo conhecimentos;
+- **Conhecimentos:** Nele apresentamos meus conhecimentos em algumas linguagens como o foco no front-end;
+
+---
+
+## 💼 Tecnologias utilizadas
+Para o desenvolvimento deste site utilizei as seguintes tecnologias:
+
+- HTML;
+- Sass;
+- JavaScript;
+- ScrollReveal;
+
+---
+
+## 🦄 Autora<br>
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/bea-ferraz">
+        <img src="assets/image/avatarbea.jpeg" width="100px;" alt="Foto da Beatriz"/><br>
+        <sub>
+          <b>Beatriz Ferraz</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
---
+++ assets/js/main.js
@@ -0,0 +1,114 @@
+//Menu
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
+
+//Maquina de escrever
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 100 * i);
+  });
+}
+const titulo = document.querySelector(".maquina-escrever");
+typeWrite(titulo);
+
+//Revelar animação
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: 2000,
+  reset: true,
+});
+
+//Home delay
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".linguagensProjeto", { delay: 400 });
+sr.reveal(".imagem-espaco-delay", { delay: 200 });
+sr.reveal(".imagem-iuri-delay", { delay: 600 });
+sr.reveal(".scroll-delay", { delay: 1000 });
+//Sobre mim delay
+sr.reveal(".imagem-delay", {});
+sr.reveal(".titulo-delay", { delay: 200 });
+sr.reveal(".descricao-delay", { delay: 400 });
+sr.reveal(".cards-interval", { interval: 400 });
+
+//Animação conhecimentos
+var texto1 = document.querySelector(".mudarTextoDescricao");
+
+document.querySelector(".html").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir. <br><br> 8 meses de experiência";
+});
+
+document.querySelector(".css").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML. <br><br> 8 meses de experiência";
+});
+
+document.querySelector(".js").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. <br><br> 5 meses de experiência";
+});
+
+document.querySelector(".sass").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "Sass (o pré-processador) é uma linguagem de folhas de estilo. É uma simples linguagem de script usada em arquivos Sass. <br><br> 1 mese de experiência";
+});
+
+document.querySelector(".react").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. <br><br> 2 meses de experiência";
+});
+
+
+
+var sairCaixa = document.getElementsByClassName("sairCaixa");
+
+for (i = 0; i < sairCaixa.length; i++) {
+  sairCaixa[i].addEventListener("mouseout", function mudarTextoNormal() {
+    texto1.innerHTML = `Estou convencida de que juntos alcançaremos coisas incríveis.💜<br><br>*passe o mouse no card para ler sobre*`;
+  });
+}
