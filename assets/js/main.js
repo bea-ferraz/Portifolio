e42424288b02bdd5ce03e1fad55726a8ff60f85a
Beatriz Ferraz <79225626+bea-ferraz@users.noreply.github.com>
2025-03-27 12:46:01 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,32 +80,32 @@ var texto1 = document.querySelector(".mudarTextoDescricao");
 
 document.querySelector(".html").addEventListener("mouseover", function () {
   texto1.innerHTML =
-    "HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir. <br><br> 2 anos de experiência";
+    "HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir. <br><br> 4 anos de experiência";
 });
 
 document.querySelector(".css").addEventListener("mouseover", function () {
   texto1.innerHTML =
-    "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML. <br><br> 2 anos de experiência";
+    "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML. <br><br> 4 anos de experiência";
 });
 
 document.querySelector(".js").addEventListener("mouseover", function () {
   texto1.innerHTML =
-    "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. <br><br> 2 anos de experiência";
+    "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. <br><br> 3 anos de experiência";
 });
 
 document.querySelector(".sass").addEventListener("mouseover", function () {
   texto1.innerHTML =
-    "Sass (o pré-processador) é uma linguagem de folhas de estilo. É uma simples linguagem de script usada em arquivos Sass. <br><br> 2 anos de experiência";
+    "Sass (o pré-processador) é uma linguagem de folhas de estilo. É uma simples linguagem de script usada em arquivos Sass. <br><br> 3 anos de experiência";
 });
 
 document.querySelector(".angular").addEventListener("mouseover", function () {
   texto1.innerHTML =
-    "Angular é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. <br><br> 1 ano de experiência";
+    "Angular é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. <br><br> 2 ano de experiência";
 });
 
 document.querySelector(".react").addEventListener("mouseover", function () {
   texto1.innerHTML =
-    "React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. <br><br> 2 meses de experiência";
+    "WordPress é uma plataforma de gerenciamento de conteúdo (CMS) de código aberto, amplamente utilizada para criar sites e blogs. <br><br> 4 meses de experiência";
 });
 var sairCaixa = document.getElementsByClassName("sairCaixa");
 
